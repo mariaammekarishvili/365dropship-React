113cfe191bcb19d551d59a9c6c316f65ee9fd648
MariamMEK <mariam.mekarishvili.1@btu.edu.ge>
2021-05-12 19:01:46 +0400

    header button + text

--- src/Header.js
+++ src/Header.js
@@ -1,8 +1,12 @@
 import SearchBox from "./SearchBox";
+import Button from "./Button";
+import Paragraph from "./Paragraph";
 
 const Header = () =>{
     return(
         <header className="header">
+            <Button title={'SELECT ALL'}/>
+            <Paragraph text={'selected 50 out of 270.872 products'} class={'Header__para'}/>
             <SearchBox/>
         </header>
     )
---
+++ src/Paragraph.js
@@ -0,0 +1,7 @@
+
+const Paragraph = (props) => {
+    return(
+        <p className={props.class}>{props.text}</p>
+    )
+}
+export default Paragraph
