6948a7380f28d0f0b4d1f46ab28295da1d8b4f75
MariamMEK <mariam.mekarishvili.1@btu.edu.ge>
2021-07-31 18:57:29 +0400

    map is done

--- src/AboutPage/AboutPage.js
+++ src/AboutPage/AboutPage.js
@@ -1,10 +1,17 @@
+import '../CSS/AboutPage.css'
+import {AboutPageMap} from "./AboutPageMap";
+import Navigation from "../common/Navigation";
+import {Hidden} from "@material-ui/core";
 
 export const AboutPage = () => {
     return(
         <>
-            <iframe className={'google-map'}
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2977.6598913198563!2d44.77089771480924!3d41.72785738278299!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4044733841246043%3A0xf302eddfb9eb743d!2z4YOV4YOU4YOR4YOY4YOWIOGDoeGDkOGDpeGDkOGDoOGDl-GDleGDlOGDmuGDnQ!5e0!3m2!1ska!2sge!4v1627742595445!5m2!1ska!2sge">
-            </iframe>
+            <Hidden xsDown><Navigation/></Hidden>
+            <div className={'about-page'}>
+
+                <AboutPageMap/>
+            </div>
+
         </>
     )
 }
